2450cd103c5ba0be68a62a54354f77c98b560be8
IntegratedQuantum <jahe788@gmail.com>
2025-11-15 13:10:09 +0100

    Only print errors

--- compress.bash
+++ compress.bash
@@ -5,6 +5,6 @@ rm -r output
 for item in `find music | grep ".*\.ogg\|.*\.mp3|.*\.qoa|.*\.wav|.*\.flac"`
 do
 	mkdir -p output/${item%/*}
-	ffmpeg -i $item -b:a 128k output/$item
+	ffmpeg -i $item -b:a 128k output/$item -v error
 done
 
