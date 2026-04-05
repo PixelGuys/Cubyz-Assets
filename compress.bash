d0146fa3e94d949177df61bfb26102999b422b5c
IntegratedQuantum <jahe788@gmail.com>
2026-04-05 11:33:47 +0200

    Fix mistake in the CI script

--- compress.bash
+++ compress.bash
@@ -2,7 +2,7 @@
 
 rm -r output
 
-for item in `find music | grep ".*\.ogg\|.*\.mp3|.*\.qoa|.*\.wav|.*\.flac"`
+for item in `find music | grep ".*\.ogg\|.*\.mp3\|.*\.qoa\|.*\.wav\|.*\.flac"`
 do
 	mkdir -p output/${item%/*}
 	ffmpeg -i $item -b:a 128k output/$item -v error
