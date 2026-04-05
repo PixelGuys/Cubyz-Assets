57e0351544560eb0608f31766237ed102ef4a086
IntegratedQuantum <jahe788@gmail.com>
2026-04-05 12:04:53 +0200

    More fixes to the CI scripts and removed unconvertible music, see #26

--- compress.bash
+++ compress.bash
@@ -4,8 +4,9 @@ rm -r output
 
 for item in `find music | grep ".*\.ogg\|.*\.mp3\|.*\.qoa\|.*\.wav\|.*\.flac"`
 do
+	echo $item
 	mkdir -p output/${item%/*}
-	ffmpeg -i $item -b:a 128k output/$item -v error
+	ffmpeg -i $item -b:a 128k output/${item%.*}.ogg -v error
 done
 
 cp -r fonts output/fonts
